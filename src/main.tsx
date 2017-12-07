5fc3151ffb0937c9a4a6281de9615f442f023e2e
Shantanu Raj <s@sraj.me>
2017-12-08 03:44:59 +0530

    Add built file to git, use inner screen dimens

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import * as ReactDOM from 'react-dom';
 import data from './data';
 
 ReactDOM.render(
-  <App width={window.screen.availWidth} height={window.screen.availHeight} data={data} />,
+  <App width={window.innerWidth} height={window.innerHeight} data={data} />,
   document.getElementById('app'),
 );
